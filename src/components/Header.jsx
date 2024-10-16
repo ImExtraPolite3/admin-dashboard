f54c73ff5bb4f64b1cff9514c263c78018491528
ExtraPolite <imextrapolite2@gmail.com>
2024-10-16 14:55:48 -0400

    add Header page and export to App

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+const allButtonNames = ['New', 'Upload', 'Share'];
+
+function DisplayButtons() {
+  return allButtonNames.map((button) => {
+    return <button key={button}>{button}</button>;
+  });
+}
+
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <img src="magnify.svg" alt="" width="20px" />
+        <input type="text" />
+        <img src="bell-ring-outline.svg" alt="" width="20px" />
+      </div>
+      <div>
+        <img src="emoticon-poop.svg" alt="" width="40px" />
+        <div>
+          <p>Hi there,</p>
+          <h1>Random Person (@SomeRandomPerson)</h1>
+        </div>
+        <div>
+          <DisplayButtons />
+        </div>
+      </div>
+    </header>
+  );
+}
