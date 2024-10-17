dd4e07ab4ee57bb92653a7404597cabc6f74a15a
ExtraPolite <imextrapolite2@gmail.com>
2024-10-17 06:57:06 -0400

    create Projects file and import to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Sidebar from './components/Sidebar.';
 import Header from './components/Header';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <>
       <Sidebar />
       <Header />
+      <Projects />
     </>
   );
 }
