5279e67baae77b57426b78c63cddd491107c6f72
ExtraPolite <imextrapolite2@gmail.com>
2024-10-17 11:57:14 -0400

    use .fill in order make multiple of the same component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,3 +1,27 @@
+function Project() {
+  return (
+    <>
+      <h3>Super Cool Project</h3>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. At vero nobis
+        dolores aspernatur rem optio nam doloremque. Nam tempora dicta ab fugit
+        aliquid possimus a voluptatem, magni architecto recusandae quasi.
+      </p>
+    </>
+  );
+}
+
 export default function Projects() {
-  return <section></section>;
+  return (
+    <section>
+      <h2>Your Projects</h2>
+      <>
+        {Array(6)
+          .fill(true)
+          .map((_, i) => (
+            <Project key={i} />
+          ))}
+      </>
+    </section>
+  );
 }
