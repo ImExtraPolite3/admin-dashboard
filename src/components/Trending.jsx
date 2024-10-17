2ad78f14659ef8d5b23a32fc5d0818673836408a
ExtraPolite <imextrapolite2@gmail.com>
2024-10-17 17:11:50 -0400

    add all parts to Trending page

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,7 +1,26 @@
+function DisplayTrending() {
+  return (
+    <div>
+      <img src="emoticon-poop.svg" alt="" width="30px" />
+      <div>
+        <p>@name</p>
+        <p>something</p>
+      </div>
+    </div>
+  );
+}
+
 export default function Trending() {
   return (
     <section className="trending">
       <h4>Trending</h4>
+      <div>
+        {Array(4)
+          .fill(true)
+          .map((_, i) => (
+            <DisplayTrending key={i} />
+          ))}
+      </div>
     </section>
   );
 }
