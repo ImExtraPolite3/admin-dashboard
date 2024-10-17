d27ba502c2a68a95359b027e6e3f663039ed09bb
ExtraPolite <imextrapolite2@gmail.com>
2024-10-17 16:45:01 -0400

    add all parts for Announcement page

--- src/components/Announcements.jsx
+++ src/components/Announcements.jsx
@@ -1,3 +1,31 @@
+function DisplayAnnouncements() {
+  return (
+    <div>
+      <h4>Site Maintanence</h4>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis
+        ratione natus dicta expedita veritatis at laboriosam iure culpa quod
+        non.
+      </p>
+    </div>
+  );
+}
+
 export default function Announcements() {
-  return <section className="announcements"></section>;
+  return (
+    <section className="announcements">
+      <h3>Announcements</h3>
+      <>
+        {Array(2)
+          .fill(true)
+          .map((_, i) => (
+            <>
+              <DisplayAnnouncements key={i} />
+              <hr />
+            </>
+          ))}
+        <DisplayAnnouncements key={'3'} />
+      </>
+    </section>
+  );
 }
