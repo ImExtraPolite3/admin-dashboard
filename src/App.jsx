54e609a0e78084b61e4e5898b990727c337dd9f9
ExtraPolite <imextrapolite2@gmail.com>
2024-10-24 00:04:15 -0400

    fix Sidebar link in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Sidebar from './components/Sidebar.';
+import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 import Projects from './components/Projects';
 import Announcements from './components/Announcements';
